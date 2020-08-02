b988199e053672e31a9aca7767bec6d4f03cee23
maza100680 <69115752+maza100680@users.noreply.github.com>
2020-08-02 12:46:03 -0500

    Add files via upload

---
+++ tienda-online/gracias.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+require 'funciones.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Tienda Online</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <link rel="stylesheet" href="assets/css/whatssap.css">
+  </head>
+
+  <body>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20" class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Tienda Online</a>
+        </div>
+        
+      </div>
+    </nav>
+
+    <div class="container" id="main">
+        <div class="row">
+            <div class="jumbotron">
+                <p>Para completar su compra comuniquese con el vendedor presionando el icono de whatsapp</p>
+                <p>
+                    <a href="index.php">Regresar</a>
+                </p>
+            </div>
+
+
+
+        </div>
+
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
